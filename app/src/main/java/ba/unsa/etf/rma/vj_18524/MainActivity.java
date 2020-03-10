7d35343d6ae2cc2254cea8d10d69a38c50ae1a8e
rtomas1 <rtomas1@etf.unsa.ba>
2020-03-10 16:28:30 +0100

    initial commit

---
+++ app/src/main/java/ba/unsa/etf/rma/vj_18524/MainActivity.java
@@ -0,0 +1,42 @@
+package ba.unsa.etf.rma.vj_18524;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText editText;
+    private ListView listView;
+    private ArrayList<String> entries;
+    private ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editText);
+        listView = (ListView) findViewById(R.id.listView);
+
+        entries =new ArrayList<String>();
+        adapter =new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, entries);
+        listView.setAdapter(adapter);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                entries.add(0,editText.getText().toString());
+                adapter.notifyDataSetChanged();
+                editText.setText("");
+            }
+        });
+    }
+}
